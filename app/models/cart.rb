868053c90ffa66de00b0c353876d45b076c87e97
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2021-07-07 19:13:05 +0530

    Make cart smarter

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,21 @@
 class Cart < ApplicationRecord
     has_many :line_items, dependent: :destroy
+
+    def add_product(product)
+      current_item = line_items.find_by(product_id: product.id)
+      if current_item
+        current_item.quantity += 1
+      else
+        current_item = line_items.build(product_id:product.id)
+      end
+      current_item
+    end
+
+    def total_price
+      result = 0
+      line_items.each do |line_item|
+        result += line_item.total_price
+      end
+      result
+    end
 end
