c7f37a74979cbea69ecfed0be0687f95a98516fe
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2021-07-13 06:59:53 +0530

    Add react

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ApplicationRecord
     has_many :line_items, dependent: :destroy
     enum pay_type: {
         "Check" => 0,
-        "Credit Card" => 1,
-        "Purchase Order" => 2
+        "Credit card" => 1,
+        "Purchase order" => 2
     }
     validates :name, :address, :email, presence: true
     validates :pay_type, inclusion: pay_types.keys
