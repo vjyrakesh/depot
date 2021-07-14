85832d7bf561a185c1f89598efb9eee7b6365d33
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2021-07-14 18:28:46 +0530

    Add login

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  skip_before_action :authorize
   include CurrentCart
   before_action :set_order, only: %i[ show edit update destroy ]
   before_action :set_cart, only: [:new, :create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_010411) do
+ActiveRecord::Schema.define(version: 2021_07_13_013833) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2021_07_11_010411) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
