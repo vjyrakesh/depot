49dac6d6bde6592bbffbe1237935f6654054a031
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2025-11-25 13:25:09 +0000

    Add upto auth

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :name, uniqueness: true
+  has_secure_password
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_20_133919) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_23_111021) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_20_133919) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
